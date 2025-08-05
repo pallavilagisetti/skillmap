49b8756498985ebe990225e5d5b9acd236155fb3
Pallavi Lagisetti <pallavilagisetti2003@gmail.com>
2025-08-05 22:16:52 +0530

    Fix login bug and update user dashboard

--- map/src/components/ResumeUploader.jsx
+++ map/src/components/ResumeUploader.jsx
@@ -31,7 +31,7 @@ const ResumeUploader = () => {
     formData.append("resume", file);
 
     try {
-      const response = await fetch("http://localhost:5000/api/upload", {
+      const response = await fetch("https://skillmap-1.onrender.com/api/upload", {
         method: "POST",
         body: formData,
       });
